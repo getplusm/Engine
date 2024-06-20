795f7aa5648ab223d587e05e5dff3cd5e4281c3f
plazmer <48393153+getplusm@users.noreply.github.com>
2024-06-20 12:27:35 +0500

    added 1.21 support

--- engine/src/main/java/t/me/p1azmer/engine/Version.java
+++ engine/src/main/java/t/me/p1azmer/engine/Version.java
@@ -3,83 +3,88 @@ package t.me.p1azmer.engine;
 import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Comparator;
 import java.util.stream.Stream;
 
 public enum Version {
 
-  V1_17_R1("1.17.1", true),
-  V1_18_R2("1.18.2"),
-  @Deprecated V1_19_R1("1.19.2", true),
-  @Deprecated V1_19_R2("1.19.3", true),
-  V1_19_R3("1.19.4"),
-  V1_20_R1("1.20.1", true),
-  V1_20_R2("1.20.2", true),
-  V1_20_R3("1.20.4"),
-  MC_1_20_6("1.20.6"),
-  UNKNOWN("Unknown"),
-  ;
+    V1_17_R1("1.17.1", true),
+    V1_18_R2("1.18.2", true),
+    V1_19_R1("1.19.2"),
+    V1_19_R2("1.19.3"),
+    V1_19_R3("1.19.4"),
+    V1_20_R1("1.20.1"),
+    V1_20_R2("1.20.2"),
+    V1_20_R3("1.20.4"),
+    MC_1_20_6("1.20.6"),
+    MC_1_21("1.21"),
+    UNKNOWN("Unknown"),
+    ;
 
-  public static final String CRAFTBUKKIT_PACKAGE = Bukkit.getServer().getClass().getPackage().getName();
+    public static final String CRAFTBUKKIT_PACKAGE = Bukkit.getServer().getClass().getPackage().getName();
 
-  private static Version current;
+    private static Version current;
 
-  private final boolean deprecated;
-  private final String localized;
+    private final boolean deprecated;
+    private final String localized;
 
-  Version(@NotNull String localized) {
-    this(localized, false);
-  }
-
-  Version(@NotNull String localized, boolean deprecated) {
-    this.localized = localized;
-    this.deprecated = deprecated;
-  }
-
-  @NotNull
-  @Deprecated
-  public static String getProtocol() {
-    return Bukkit.getServer().getBukkitVersion();
-  }
-
-  @NotNull
-  public static Version getCurrent() {
-    if (current == null) {
-      String protocol = Bukkit.getServer().getBukkitVersion();
-      current = Stream.of(values()).filter(version -> protocol.startsWith(version.getLocalized())).findFirst().orElse(UNKNOWN);
+    Version(@NotNull String localized) {
+        this(localized, false);
     }
-    return current;
-  }
 
-  public boolean isDeprecated() {
-    return deprecated;
-  }
+    Version(@NotNull String localized, boolean deprecated) {
+        this.localized = localized;
+        this.deprecated = deprecated;
+    }
 
-  @NotNull
-  public String getLocalized() {
-    return localized;
-  }
+    @NotNull
+    @Deprecated
+    public static String getProtocol() {
+        return Bukkit.getServer().getBukkitVersion();
+    }
 
-  public boolean isLower(@NotNull Version version) {
-    return this.ordinal() < version.ordinal();
-  }
+    @NotNull
+    public static Version getCurrent() {
+        if (current == null) {
+            String protocol = Bukkit.getServer().getBukkitVersion();
+            current = Stream.of(values()).sorted(Comparator.reverseOrder())
+                            .filter(version -> protocol.startsWith(version.getLocalized()))
+                            .findFirst()
+                            .orElse(UNKNOWN);
+        }
+        return current;
+    }
 
-  public boolean isHigher(@NotNull Version version) {
-    return this.ordinal() > version.ordinal();
-  }
+    public boolean isDeprecated() {
+        return deprecated;
+    }
 
-  public static boolean isAtLeast(@NotNull Version version) {
-    return version.isCurrent() || getCurrent().isHigher(version);
-  }
+    @NotNull
+    public String getLocalized() {
+        return localized;
+    }
 
-  public static boolean isAbove(@NotNull Version version) {
-    return getCurrent().isHigher(version);
-  }
+    public boolean isLower(@NotNull Version version) {
+        return this.ordinal() < version.ordinal();
+    }
 
-  public static boolean isBehind(@NotNull Version version) {
-    return getCurrent().isLower(version);
-  }
+    public boolean isHigher(@NotNull Version version) {
+        return this.ordinal() > version.ordinal();
+    }
 
-  public boolean isCurrent() {
-    return this == Version.getCurrent();
-  }
+    public static boolean isAtLeast(@NotNull Version version) {
+        return version.isCurrent() || getCurrent().isHigher(version);
+    }
+
+    public static boolean isAbove(@NotNull Version version) {
+        return getCurrent().isHigher(version);
+    }
+
+    public static boolean isBehind(@NotNull Version version) {
+        return getCurrent().isLower(version);
+    }
+
+    public boolean isCurrent() {
+        return this == Version.getCurrent();
+    }
 }
